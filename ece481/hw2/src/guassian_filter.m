fa93eee2b9c3cb22292f93ab2d3cb4947860e290
mundra <mundra@zoidberg.(none)>
2010-02-13 11:30:15 -0600

    Finished computer assginment for image proc

--- ece481/hw2/src/guassian_filter.m
+++ ece481/hw2/src/guassian_filter.m
@@ -1,18 +1,17 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%          File: rbg_channel.m
+%          File: guassian_filter.m
 %        Author: Jay Mundrawala(jay@ir.iit.edu)
-%       Created: Sat Feb 6 2010
+%       Created: Sat Feb 13 2010
 %
-%   Description: This script reads the tif files and displays the original
-%                image, followed by the its RGB channels seperated.
-%
-%         Usage: This script works correctly with octave. For matlab, lines
-%         27 and 28 need to commented out.
+%   Description: This script reads an image and applys a guassian filter with
+%                various sigma values. A file with the results is saved in
+%                the data folder.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-f     = uint8(imread(input('query: ','s')));
+q     = input('file: ', 's');
+f     = uint8(imread(q));
 [r,c] = size(f);
 r = (r-1)/2;
 c = (c-1)/2;
@@ -21,22 +20,16 @@ c = (c-1)/2;
 figure;
 i = 1;
 F = fft2(f);
+imagesc(fftshift(log(abs(F).^2)));
+print('data/fft.eps', '-deps');
+
+figure;
 subplot(3,3,i); imshow(f); axis image; title('Original');
 for sig = [350 115 75 60 50 40 20 10]
     H = fftshift(exp(-(u.^2 + v.^2)/(2*sig^2)));
     g = real(ifft2(F .* H));
-    i = i + 1
+    i = i + 1;
     subplot(3,3,i); imagesc(g); axis image; title(['\sigma=', num2str(sig)]);
-    %imshow(g,[]);
 end
-print('my_plot.eps', '-deps');
-pause(100);
-%figure;
-%subplot(1,4,1), imagesc(q_img); axis image;
-%subplot(1,4,2), imagesc(q_r); axis image;
-%subplot(1,4,3), imagesc(q_g); axis image;
-%subplot(1,4,4), imagesc(q_b); axis image;
-
-%output = ["data/c", num2str(c++), ".eps"]
-%print(output, "-deps");
-%pause(1);
+print('data/plot.eps', '-deps');
+pause(1);
